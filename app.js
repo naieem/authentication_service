7c5481835fa4c72533f58ddddc552cb848aa5bbd
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-04-18 20:38:50 +0600

    es6 configured and vscode support done

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', index);
-app.use('/api', api);
+app.use('/api', api); // api routing 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
